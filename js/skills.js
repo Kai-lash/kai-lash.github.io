f5ffa56b5191b42259b80ea830faa96f0db40317
Kai-lash <kailashjoshi1104@gmail.com>
2016-10-29 23:29:14 -0400

    modified script for skills section

--- js/skills.js
+++ js/skills.js
@@ -1,4 +1,9 @@
 jQuery(document).ready(function(){
+	jQuery('.skillbar').each(function(){
+			jQuery(this).find('.skillbar-bar').animate({
+				width:jQuery(this).attr('data-percent')
+			},6000);
+	});
 	$('a[href="#skills"]').click(function(){
 		jQuery('.skillbar').each(function(){
 			jQuery(this).find('.skillbar-bar').animate({
